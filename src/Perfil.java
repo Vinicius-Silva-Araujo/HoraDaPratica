0390ffd7b1f33eebbc99107f533c4fed7c0ca214
Vinicius-Silva-Araujo <viniciussilvaaraujo93@gmail.com>
2024-01-16 22:09:19 -0300

    Initial commit

---
+++ src/Perfil.java
@@ -0,0 +1,7 @@
+public class Perfil {
+
+    public static void main(String[] args){
+        System.out.println(" Ola Vinicius");
+        System.out.println(" Tudo Bem ?");
+    }
+}
---
+++ src/Soma.java
@@ -0,0 +1,5 @@
+public class Soma {
+    public  static void main (String[] args){
+        System.out.println("Soma " + (10+5) );
+    }
+}
